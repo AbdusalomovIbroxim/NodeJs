3aaf4e78ccfd68d7e2543235a8386167e0be3fca
Ibroxim <ibrohimlololowka@gmail.com>
2024-08-19 17:39:23 +0500

    commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/userController');
+const UserController = require('../controllers/UserController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 // Защищенный маршрут
