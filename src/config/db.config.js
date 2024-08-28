fc98e82e493a1960c62dad6d204cffccd11c5065
Ibroxim <ibrohimlololowka@gmail.com>
2024-08-28 19:09:42 +0500

    commit

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -10,30 +10,30 @@ console.log({
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  logging: false,
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   dialect: 'postgres',
+//   logging: false,
   // dialectOptions: {
   //   ssl : {
   //     require: true,
   //     rejectUnauthorized: false, // This option is used for self-signed certificates
   //   },
   // },
-});
-
-
-
-// const sequelize = new Sequelize(`${process.env.DB_URL}`, {
-//   dialect: 'postgres',
-//   dialectOptions: {
-//       ssl: {
-//           require: true,
-//           rejectUnauthorized: false 
-//       }
-//   }
 // });
+
+
+
+const sequelize = new Sequelize(`${process.env.DB_URL}`, {
+  dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false 
+      }
+  }
+});
 
 
 
